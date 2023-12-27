6bd3daa25d4faa2c41953799cd804a74b614ddf3
Odgug <urmomafrog@gmail.com>
2023-12-27 14:45:00 +0200

    Create Filters working in routers - by radimous

---
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinBulkItemMatcher.java
@@ -0,0 +1,32 @@
+package net.joseph.vaultfilters.mixin;
+
+import com.simibubi.create.content.logistics.filter.FilterItem;
+import me.desht.modularrouters.logic.filter.Filter;
+import me.desht.modularrouters.logic.filter.matchers.BulkItemMatcher;
+import me.desht.modularrouters.util.SetofItemStack;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+//Create filter integration for modular routers by radimous on GitHub rizek_ on Discord, massive thanks.
+
+@Mixin(value = BulkItemMatcher.class, remap = false)
+public class MixinBulkItemMatcher {
+    @Shadow @Final private SetofItemStack stacks;
+
+    @Inject(method = "matchItem", at = @At("HEAD"), cancellable = true)
+    public void createItemMatcher(ItemStack stack, Filter.Flags flags, CallbackInfoReturnable<Boolean> cir){
+        for (var filter : this.stacks) {
+            if (filter.getItem() instanceof FilterItem) {
+                if (FilterItem.test(null, stack, filter, !flags.isIgnoreNBT())) {
+                    cir.setReturnValue(true);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinSimpleItemMatcher.java
@@ -0,0 +1,28 @@
+package net.joseph.vaultfilters.mixin;
+
+import com.simibubi.create.content.logistics.filter.FilterItem;
+import me.desht.modularrouters.logic.filter.Filter;
+import me.desht.modularrouters.logic.filter.matchers.SimpleItemMatcher;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+//Create filter integration for modular routers by radimous on GitHub rizek_ on Discord, massive thanks.
+
+@Mixin(value = SimpleItemMatcher.class, remap = false)
+public class MixinSimpleItemMatcher {
+    @Shadow
+    @Final
+    private ItemStack filterStack;
+
+    @Inject(method = "matchItem", at = @At("HEAD"), cancellable = true)
+    public void createFilterMatcher(ItemStack stack, Filter.Flags flags, CallbackInfoReturnable<Boolean> cir) {
+        if (filterStack.getItem() instanceof FilterItem) {
+            cir.setReturnValue(FilterItem.test(null, stack, this.filterStack, !flags.isIgnoreNBT()));
+        }
+    }
+}
