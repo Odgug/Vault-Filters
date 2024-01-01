768b1dd6d96181032b4b75029c61576ed359e3e1
Odgug <urmomafrog@gmail.com>
2024-01-01 18:16:18 +0200

    unidentified attribute

--- src/main/java/net/joseph/vaultfilters/ItemAttributes/ModItemAttributes.java
+++ src/main/java/net/joseph/vaultfilters/ItemAttributes/ModItemAttributes.java
@@ -5,6 +5,7 @@ import net.joseph.vaultfilters.ItemAttributes.custom.*;
 public class ModItemAttributes {
     public static void register() {
         GearRarityAttribute.register();
+        IsUnidentifiedAttribute.register();
         InscriptionRoomTypeAttribute.register();
         InscriptionRoomAttribute.register();
         InscriptionTimeAttribute.register();
