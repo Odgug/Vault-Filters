3ed2921cd3159bfbca823bdeadaabd7136174bbe
Odgug <urmomafrog@gmail.com>
2024-01-02 14:34:06 +0200

    comment

--- src/main/java/net/joseph/vaultfilters/mixin/MixinAttributeResearchBypass.java
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinAttributeResearchBypass.java
@@ -12,6 +12,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+//Create filter integration for modular routers by radimous on GitHub rizek_ on Discord, massive thanks.
+
 @Mixin(value = ResearchTree.class, remap = false)
 public class MixinAttributeResearchBypass {
     @Inject(method = "restrictedBy(Lnet/minecraft/world/item/ItemStack;Liskallia/vault/research/Restrictions$Type;)Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
