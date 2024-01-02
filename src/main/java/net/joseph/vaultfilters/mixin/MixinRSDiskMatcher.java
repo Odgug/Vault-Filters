a7f425f7f85e51e55d5b96c35b3d7cd5a59b9bd1
Odgug <urmomafrog@gmail.com>
2024-01-02 19:54:59 +0200

    rizek's disk redirect

--- src/main/java/net/joseph/vaultfilters/mixin/MixinRSDiskMatcher.java
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinRSDiskMatcher.java
@@ -12,76 +12,58 @@ import iskallia.vault.item.gear.VaultArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.ItemHandlerHelper;
+import org.checkerframework.checker.units.qual.K;
+import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+
 @Mixin(value = ItemStorageDisk.class, remap = false)
-public abstract class MixinRSDiskMatcher {
-    @Final
-    @Shadow
-    private Multimap<Item, ItemStack> stacks;
-
-    @Shadow
-    private int itemCount;
-
-    @Shadow
-    protected abstract void onChanged();
-
-    @Inject(method = "extract(Lnet/minecraft/world/item/ItemStack;IILcom/refinedmods/refinedstorage/api/util/Action;)Lnet/minecraft/world/item/ItemStack;", at = @At("HEAD"), cancellable = true)
-    public void checkFilter(ItemStack stack, int size, int flags, Action action, CallbackInfoReturnable<ItemStack> cir) {
-
-
-
-        if (stack.getItem() instanceof FilterItem) {
-            Collection<ItemStack> finalstack = stacks.get(ModItems.SMALL_CHARM);
-            finalstack.addAll(stacks.get(ModItems.LARGE_CHARM));
-            finalstack.addAll(stacks.get(ModItems.GRAND_CHARM));
-            finalstack.addAll(stacks.get(ModItems.MAJESTIC_CHARM));
-
-            finalstack.addAll(stacks.get(ModItems.TRINKET));
-            finalstack.addAll(stacks.get(ModItems.INSCRIPTION));
-            finalstack.addAll(stacks.get(ModItems.JEWEL));
-
-            finalstack.addAll(stacks.get(ModItems.CHESTPLATE));
-            finalstack.addAll(stacks.get(ModItems.HELMET));
-            finalstack.addAll(stacks.get(ModItems.BOOTS));
-            finalstack.addAll(stacks.get(ModItems.LEGGINGS));
-
-            finalstack.addAll(stacks.get(ModItems.SWORD));
-            finalstack.addAll(stacks.get(ModItems.AXE));
-
-            finalstack.addAll(stacks.get(ModItems.FOCUS));
-            finalstack.addAll(stacks.get(ModItems.WAND));
-            finalstack.addAll(stacks.get(ModItems.SHIELD));
-            for (ItemStack otherStack : finalstack) {
-                if (API.instance().getComparer().isEqual(otherStack, stack, flags)) {
-                    if (size > otherStack.getCount()) {
-                        size = otherStack.getCount();
-                    }
-
-                    if (action == Action.PERFORM) {
-                        if (otherStack.getCount() - size == 0) {
-                            stacks.remove(otherStack.getItem(), otherStack);
-                        } else {
-                            otherStack.shrink(size);
-                        }
-
-                        itemCount -= size;
-
-                        onChanged();
-                    }
-
-                    cir.setReturnValue(ItemHandlerHelper.copyStackWithSize(otherStack, size));
-                }
+public class MixinRSDiskMatcher {
+    private static final Item[] VAULT_GEAR = {
+            ModItems.HELMET,
+            ModItems.CHESTPLATE,
+            ModItems.LEGGINGS,
+            ModItems.BOOTS,
+            ModItems.SWORD,
+            ModItems.AXE,
+            ModItems.SHIELD,
+            ModItems.IDOL_BENEVOLENT,
+            ModItems.IDOL_MALEVOLENCE,
+            ModItems.IDOL_OMNISCIENT,
+            ModItems.IDOL_TIMEKEEPER,
+            ModItems.JEWEL,
+            ModItems.MAGNET,
+            ModItems.WAND,
+            ModItems.FOCUS,
+            ModItems.SMALL_CHARM,
+            ModItems.LARGE_CHARM,
+            ModItems.GRAND_CHARM,
+            ModItems.MAJESTIC_CHARM,
+            ModItems.TRINKET,
+            ModItems.INSCRIPTION,
+    };
+    @Redirect(method = "extract(Lnet/minecraft/world/item/ItemStack;IILcom/refinedmods/refinedstorage/api/util/Action;)Lnet/minecraft/world/item/ItemStack;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Multimap;get(Ljava/lang/Object;)Ljava/util/Collection;"))
+    private Collection<ItemStack> getAllItemsIfFilter(Multimap<Item, ItemStack> instance, Object item) {
+        ArrayList<ItemStack> stacks = new ArrayList<>();
+        if (item instanceof FilterItem) {
+            for (var gearPiece: VAULT_GEAR) {
+                stacks.addAll(instance.get(gearPiece));
             }
-
-            cir.setReturnValue(ItemStack.EMPTY); ;
+            return stacks;
         }
-        }
+        return instance.get((Item) item);
     }
+}
+
+
+
+
