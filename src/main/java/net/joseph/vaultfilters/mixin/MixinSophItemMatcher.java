c6412cd761d916fd1a738cffb770fc55f12ce109
Odgug <urmomafrog@gmail.com>
2024-01-05 22:55:36 +0200

    sophisticated backpack integration

---
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinSophItemMatcher.java
@@ -0,0 +1,20 @@
+package net.joseph.vaultfilters.mixin;
+
+import com.simibubi.create.content.logistics.filter.FilterItem;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.FilterLogicBase;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.IOverflowResponseUpgrade;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = FilterLogicBase.class, remap = false)
+public class MixinSophItemMatcher {
+    @Inject(method = "stackMatchesFilter", at = @At("HEAD"), cancellable = true)
+    public void sophFilterMatcher(ItemStack stack, ItemStack filterStack, CallbackInfoReturnable<Boolean> cir) {
+        if (filterStack.getItem() instanceof FilterItem) {
+            cir.setReturnValue(FilterItem.test(null,stack, filterStack));
+        }
+    }
+}
