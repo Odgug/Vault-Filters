89592dea1323e6cc28f40588876386c98ec6129b
Odgug <urmomafrog@gmail.com>
2024-01-05 16:45:47 +0200

    mixin

--- src/main/java/net/joseph/vaultfilters/mixin/MixinRSDiskMatcher.java
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinRSDiskMatcher.java
@@ -1,14 +1,26 @@
 package net.joseph.vaultfilters.mixin;
 
 import com.google.common.collect.Multimap;
+import com.refinedmods.refinedstorage.api.util.Action;
+import com.refinedmods.refinedstorage.apiimpl.API;
 import com.refinedmods.refinedstorage.apiimpl.storage.disk.ItemStorageDisk;
+import com.refinedmods.refinedstorage.apiimpl.util.Comparer;
 import com.simibubi.create.content.logistics.filter.FilterItem;
+import iskallia.vault.gear.item.VaultGearItem;
 import iskallia.vault.init.ModItems;
+import iskallia.vault.item.gear.VaultArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.ItemHandlerHelper;
+import org.checkerframework.checker.units.qual.K;
+import org.lwjgl.system.CallbackI;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -52,7 +64,3 @@ public class MixinRSDiskMatcher {
         return instance.get((Item) item);
     }
 }
-
-
-
-
