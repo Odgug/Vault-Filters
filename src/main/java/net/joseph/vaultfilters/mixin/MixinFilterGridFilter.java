dbd4dcc689bee1fa6bde229c9c9534a35f3b6f9f
radimous <jurda92@gmail.com>
2024-01-31 15:06:30 +0100

    create filters work in rs filters in grids

---
+++ src/main/java/net/joseph/vaultfilters/mixin/MixinFilterGridFilter.java
@@ -0,0 +1,20 @@
+package net.joseph.vaultfilters.mixin;
+
+import com.refinedmods.refinedstorage.screen.grid.filtering.FilterGridFilter;
+import com.simibubi.create.content.logistics.filter.FilterItem;
+import net.joseph.vaultfilters.vaultfilters;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(value = FilterGridFilter.class, remap = false)
+public class MixinFilterGridFilter {
+    @ModifyArg(method = "test(Lcom/refinedmods/refinedstorage/screen/grid/stack/IGridStack;)Z", at = @At(value = "INVOKE", target = "Lcom/refinedmods/refinedstorage/api/util/IComparer;isEqual(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;I)Z"))
+    private int modifyCompareFlag(ItemStack itemStack, ItemStack filterItemStack, int compare) {
+        if (filterItemStack.getItem() instanceof FilterItem) {
+            return vaultfilters.CHECK_FILTER_FLAG;
+        }
+        return compare;
+    }
+}
