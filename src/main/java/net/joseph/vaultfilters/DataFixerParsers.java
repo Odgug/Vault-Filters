a2cefdff09b1c3f6038956b6881964beeea707eb
Odgug <urmomafrog@gmail.com>
2024-04-17 15:08:20 +0300

    fix

--- src/main/java/net/joseph/vaultfilters/DataFixerParsers.java
+++ src/main/java/net/joseph/vaultfilters/DataFixerParsers.java
@@ -13,28 +13,31 @@ public class DataFixerParsers {
     public static double getDoubleValue(String modifier) {
         int firstSpace = modifier.indexOf(' ');
         int lastSpace = modifier.lastIndexOf(' ');
+        if (firstSpace == lastSpace) {
+            return 1;
+        }
         int startIndex = 0;
-       if (modifier.contains("Cloud")) {
-           String cloudNumber = modifier.substring(firstSpace+1,lastSpace-1);
-           return switch (cloudNumber) {
-               case "II" -> 2;
-               case "III" -> 3;
-               case "IV" -> 4;
-               case "V" -> 5;
-               default -> 1;
-           };
-       }
-       boolean containsPrecentage = modifier.contains("%");
-       if (containsPrecentage) {
-           firstSpace = firstSpace-1;
-       }
-       if (modifier.charAt(0) == '+') {
-           startIndex = 1;
-       }
-        String number = modifier.substring(startIndex,firstSpace-1);
-       if (number.isBlank()) {
-           return 1;
-       }
-       return containsPrecentage ? Double.parseDouble(number) / 100 : Double.parseDouble(number);
+        if (modifier.contains("Cloud")) {
+            String cloudNumber = modifier.substring(firstSpace+1,lastSpace);
+            return switch (cloudNumber) {
+                case "II" -> 2;
+                case "III" -> 3;
+                case "IV" -> 4;
+                case "V" -> 5;
+                default -> 1;
+            };
+        }
+        boolean containsPrecentage = modifier.contains("%");
+        if (containsPrecentage) {
+            firstSpace = firstSpace-1;
+        }
+        if (modifier.charAt(0) == '+') {
+            startIndex = 1;
+        }
+        String number = modifier.substring(startIndex,firstSpace);
+        if (number.isBlank()) {
+            return 1;
+        }
+        return containsPrecentage ? Double.parseDouble(number) / 100 : Double.parseDouble(number);
     }
 }
