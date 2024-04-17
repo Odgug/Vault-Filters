00c1fa640aba5b643fac7b69bb7738b58e7f006b
Odgug <urmomafrog@gmail.com>
2024-04-17 15:05:00 +0300

    value parser done (only as double, dictate save type and convert later

---
+++ src/main/java/net/joseph/vaultfilters/DataFixerParsers.java
@@ -0,0 +1,40 @@
+package net.joseph.vaultfilters;
+
+public class DataFixerParsers {
+    public static boolean isNumber(String num) {
+        char c = num.charAt(0);
+        int ascii = c;
+        if (ascii >= 48 && ascii <= 57) {
+            return true;
+        }
+        return false;
+    }
+
+    public static double getDoubleValue(String modifier) {
+        int firstSpace = modifier.indexOf(' ');
+        int lastSpace = modifier.lastIndexOf(' ');
+        int startIndex = 0;
+       if (modifier.contains("Cloud")) {
+           String cloudNumber = modifier.substring(firstSpace+1,lastSpace-1);
+           return switch (cloudNumber) {
+               case "II" -> 2;
+               case "III" -> 3;
+               case "IV" -> 4;
+               case "V" -> 5;
+               default -> 1;
+           };
+       }
+       boolean containsPrecentage = modifier.contains("%");
+       if (containsPrecentage) {
+           firstSpace = firstSpace-1;
+       }
+       if (modifier.charAt(0) == '+') {
+           startIndex = 1;
+       }
+        String number = modifier.substring(startIndex,firstSpace-1);
+       if (number.isBlank()) {
+           return 1;
+       }
+       return containsPrecentage ? Double.parseDouble(number) / 100 : Double.parseDouble(number);
+    }
+}
