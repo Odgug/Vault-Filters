77129fce36df632bb9d1566687dcf7cf0792dcbb
Odgug <urmomafrog@gmail.com>
2024-06-16 01:41:27 +0300

    Revert "multi cache"
    This reverts commit be144e54340d26b76146b9ca6cd49e1e7e70a736.

--- src/main/java/net/joseph/vaultfilters/caching/FilterCacheValue.java
+++ src/main/java/net/joseph/vaultfilters/caching/FilterCacheValue.java
@@ -1,69 +1,40 @@
 package net.joseph.vaultfilters.caching;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.IntArrayTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class FilterCacheValue {
-    private final List<HashEntry> hashEntries;
-    public static int maxHashes = 2;
-    public FilterCacheValue(List<HashEntry> hashEntries) {
-        this.hashEntries = hashEntries;
+    private final int hash;
+    private final byte value;
+
+    public FilterCacheValue(int hash, byte value) {
+        this.hash = hash;
+        this.value = value;
     }
 
-    public List<HashEntry> getHashEntries() {
-        return hashEntries;
+    public int getHash() {
+        return hash;
     }
 
+    public byte getValue() {
+        return value;
+    }
     public static FilterCacheValue fromTag(Tag tag) {
         if (tag instanceof CompoundTag) {
             CompoundTag compoundTag = (CompoundTag) tag;
-            ListTag listTag = compoundTag.getList("hashEntries", Tag.TAG_COMPOUND);
-            List<HashEntry> hashEntries = new ArrayList<>();
-            for (Tag entryTag : listTag) {
-                CompoundTag entryCompoundTag = (CompoundTag) entryTag;
-                int hash = entryCompoundTag.getInt("hash");
-                byte value = entryCompoundTag.getByte("value");
-                hashEntries.add(new HashEntry(hash, value));
-            }
-            return new FilterCacheValue(hashEntries);
+            int hash = compoundTag.getInt("hash");
+            byte value = compoundTag.getByte("value");
+            return new FilterCacheValue(hash, value);
         }
-        return new FilterCacheValue(new ArrayList<>()); // Default or error case
+        // Handle case where tag is not of expected type (though it should be)
+        return new FilterCacheValue(0, (byte) 0); // Default or error case
     }
 
+    // Method to convert FilterCacheValue to a Tag
     public Tag toTag() {
         CompoundTag tag = new CompoundTag();
-        ListTag listTag = new ListTag();
-        for (HashEntry entry : hashEntries) {
-            CompoundTag entryTag = new CompoundTag();
-            entryTag.putInt("hash", entry.getHash());
-            entryTag.putByte("value", entry.getValue());
-            listTag.add(entryTag);
-        }
-        tag.put("hashEntries", listTag);
+        tag.putInt("hash", this.hash);
+        tag.putByte("value", this.value);
         return tag;
-    }
-
-    public static class HashEntry {
-        private final int hash;
-        private final byte value;
-
-        public HashEntry(int hash, byte value) {
-            this.hash = hash;
-            this.value = value;
-        }
-
-        public int getHash() {
-            return hash;
-        }
-
-        public byte getValue() {
-            return value;
-        }
     }
 }
