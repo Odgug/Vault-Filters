e43a4c292e510c80124cf60f0179aac4805955eb
ohad <urmomafrog@gmail.com>
2024-06-19 17:05:25 +0300

    create caching

---
+++ src/main/java/net/joseph/vaultfilters/mixin/compat/MixinCreateFilteringBehaviour.java
@@ -0,0 +1,27 @@
+package net.joseph.vaultfilters.mixin.compat;
+
+import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringBehaviour;
+import net.joseph.vaultfilters.VaultFilters;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = FilteringBehaviour.class, remap = false)
+public class MixinCreateFilteringBehaviour {
+    @Shadow
+    public boolean isActive() {return true;}
+
+    @Shadow
+    private ItemStack filter;
+
+
+
+    @Inject(method = "test(Lnet/minecraft/world/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true)
+    public void checkFilter(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!isActive() || this.filter.isEmpty() || VaultFilters.checkFilter(stack,this.filter,true));
+    }
+
+}
