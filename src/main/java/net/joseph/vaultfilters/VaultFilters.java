18b5c682a5f934dc11679d614e9edfedebab3af1
joseph <urmomafrog@gmail.com>
2024-07-29 20:02:40 +0300

    0.5.1b changes

--- src/main/java/net/joseph/vaultfilters/VaultFilters.java
+++ src/main/java/net/joseph/vaultfilters/VaultFilters.java
@@ -2,7 +2,6 @@ package net.joseph.vaultfilters;
 
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.logistics.filter.FilterItem;
-import com.simibubi.create.content.logistics.filter.FilterItemStack;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.gear.data.GearDataCache;
 import iskallia.vault.gear.item.VaultGearItem;
@@ -158,7 +157,7 @@ public class VaultFilters {
 
 
     }
-    public static boolean checkFilter(ItemStack stack, Object filterStack, boolean useCache, Level level) {
+    public static boolean checkFilter(ItemStack stack, ItemStack filterStack, boolean useCache, Level level) {
         if (!useCache) {
             return basicFilterTest(stack,filterStack,level);
         }
@@ -168,7 +167,7 @@ public class VaultFilters {
                 stackItem instanceof TrinketItem)) {
             return basicFilterTest(stack,filterStack,level);
         }
-        if (VFServerConfig.CACHE_DATAFIX.get() && filterStack instanceof ItemStack) {
+        if (VFServerConfig.CACHE_DATAFIX.get()) {
             DataFixers.clearNBTCache(stack);
         }
         //if (filterStack.getDisplayName().getString().equals("Ignore Caching")) {
@@ -187,7 +186,7 @@ public class VaultFilters {
 
 
     public static String filterKey = "hashes";
-    public static boolean basicFilterTest(ItemStack stack, Object filterStack, Level level) {
+    public static boolean basicFilterTest(ItemStack stack, ItemStack filterStack, Level level) {
 
 
         if (level == null) {
@@ -197,15 +196,7 @@ public class VaultFilters {
             }
 
         }
-        if (filterStack instanceof ItemStack stackFilter) {
-            return FilterItemStack.of(stackFilter).test(level,stack);
-        }
-        if (filterStack instanceof FilterItemStack filterItemStack) {
-            return filterItemStack.test(level,stack);
-        }
-        VaultFilters.LOGGER.debug("invalid filter entered");
-        return false;
-
+        return FilterItem.test(level,stack, filterStack);
     }
     private static boolean cacheTest(ItemStack stack, ItemStack filterStack, int maxHashes, Level level) {
         CompoundTag tag = stack.getOrCreateTag();
