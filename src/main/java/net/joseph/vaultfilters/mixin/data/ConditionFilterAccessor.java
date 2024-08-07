92e07b414c6e4fe0b969971588e2af28db32ac87
joseph <urmomafrog@gmail.com>
2024-08-07 06:38:06 +0300

    CardCondition Attribute

--- src/main/java/net/joseph/vaultfilters/mixin/data/ConditionFilterAccessor.java
+++ src/main/java/net/joseph/vaultfilters/mixin/data/ConditionFilterAccessor.java
@@ -1,15 +1,27 @@
 package net.joseph.vaultfilters.mixin.data;
 
 import iskallia.vault.core.card.CardCondition;
+import iskallia.vault.core.card.CardEntry;
+import iskallia.vault.core.card.CardNeighborType;
 import iskallia.vault.core.card.CardScaler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-@Mixin(CardCondition.class)
+@Mixin(CardCondition.Filter.class)
 public interface ConditionFilterAccessor {
     @Accessor
-    Map<Integer, List<CardScaler.Filter>> getFilters();
+    Set<CardNeighborType> getNeighborFilter();
+    @Accessor
+    Set<Integer> getTierFilter();
+    @Accessor
+    Set<CardEntry.Color> getColorFilter();
+    @Accessor
+    Set<String> getGroupFilter();
+    @Accessor
+    Integer getMinCount();
+
+    @Accessor
+    Integer getMaxCount();
 }
