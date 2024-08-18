a3957b4ac8021a1696e578c1104e919ea1913ca1
joseph <urmomafrog@gmail.com>
2024-08-18 07:50:01 +0300

    logger

--- src/main/java/net/joseph/vaultfilters/mixin/compat/create/MixinFilterItemLegacy.java
+++ src/main/java/net/joseph/vaultfilters/mixin/compat/create/MixinFilterItemLegacy.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,6 +20,8 @@ import java.lang.reflect.Method;
 @Mixin(value = FilterItem.class)
 public class MixinFilterItemLegacy {
 
+    @Unique
+    private static Method testMethodMatchNBT;
 
     private static boolean testDirect(ItemStack filter, ItemStack stack, boolean matchNBT) {
         return matchNBT ? ItemHandlerHelper.canItemStacksStack(filter, stack) : ItemStack.isSame(filter, stack);
@@ -28,19 +29,42 @@ public class MixinFilterItemLegacy {
 
     @Inject(method = "test(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;Z)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getBoolean(Ljava/lang/String;)Z", ordinal = 1, shift = At.Shift.AFTER, remap = true), cancellable = true, remap = false)
     private static void modifyLegacyTestMethod(Level world, ItemStack stack, ItemStack filter, boolean matchNBT, CallbackInfoReturnable<Boolean> cir,
-                                         @Local ItemStackHandler filterItems,
-                                         @Local(ordinal = 1) boolean respectNBT,
-                                         @Local(ordinal = 2) boolean blacklist) {
+                                               @Local ItemStackHandler filterItems,
+                                               @Local(ordinal = 1) boolean respectNBT,
+                                               @Local(ordinal = 2) boolean blacklist) {
         VaultFilters.LOGGER.info("reached mixin");
         boolean matchAll = !filter.hasTag() ? false : filter.getTag().getBoolean("MatchAll");
         if (matchAll) {
             VaultFilters.LOGGER.info("matchAll returned true");
-            boolean newIsEmpty = false;
+            if (testMethodMatchNBT == null) {
+                // try to find the method
+                try {
+                    testMethodMatchNBT = FilterItem.class.getMethod("test", Level.class, ItemStack.class, ItemStack.class, boolean.class);
+                } catch (NoSuchMethodException e) {
+                    VaultFilters.LOGGER.error("[0.5.1.b-e] could not find test method", e);
+                    // wrap it in unchecked exception
+                    throw new IllegalStateException(e);
+                }
+                VaultFilters.LOGGER.info("testMethod was defined");
+            }
+            boolean newIsEmpty = true;
             for (int slot = 0; slot < filterItems.getSlots(); ++slot) {
                 ItemStack stackInSlot = filterItems.getStackInSlot(slot);
+                VaultFilters.LOGGER.info("going over item");
                 if (!stackInSlot.isEmpty()) {
                     newIsEmpty = false;
-                    boolean matches = FilterItem.test(world,stack,stackInSlot,respectNBT);
+                    //boolean matches = FilterItem.test(world, stack, stackInSlot, respectNBT);
+                    boolean matches;
+                    try {
+                        matches = (boolean) testMethodMatchNBT.invoke(null, world, stack, stackInSlot,respectNBT);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        VaultFilters.LOGGER.error("[0.5.1.b-e] could not invoke test method", e);
+                        // wrap it in unchecked exception
+                        throw new IllegalStateException(e);
+
+                    }
+                    VaultFilters.LOGGER.info("item returned " + matches);
+                    VaultFilters.LOGGER.info("blacklist is " + blacklist);
                     if (!matches) {
                         cir.setReturnValue(blacklist);
                     }
