231150afad3f485b58e2702ab6847f63250009b2
joseph <urmomafrog@gmail.com>
2024-08-18 06:40:52 +0300

    unused imports

--- src/main/java/net/joseph/vaultfilters/mixin/compat/create/MixinFilterItemLegacy.java
+++ src/main/java/net/joseph/vaultfilters/mixin/compat/create/MixinFilterItemLegacy.java
@@ -2,23 +2,17 @@ package net.joseph.vaultfilters.mixin.compat.create;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.logistics.filter.FilterItem;
-import com.simibubi.create.content.logistics.filter.FilterItemStack;
 import net.joseph.vaultfilters.VFTests;
 import net.joseph.vaultfilters.VaultFilters;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import static net.joseph.vaultfilters.VFTests.testMethodMatchNBT;
 
