5130f212a3c107d70400fd16c72e8ce4b1c2586c
ShiftTheDev <jorge_pires_12@hotmail.com>
2024-09-17 14:02:53 +0100

    Added check for empty backpacks

---
+++ src/main/java/net/joseph/vaultfilters/attributes/backpacks/IsEmptyAttribute.java
@@ -0,0 +1,47 @@
+package net.joseph.vaultfilters.attributes.backpacks;
+
+import net.joseph.vaultfilters.attributes.abstracts.BooleanAttribute;
+import net.joseph.vaultfilters.mixin.compat.sophisticatedcore.mixin.InventoryHandlerSlotTrackerAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedbackpacks.api.CapabilityBackpackWrapper;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class IsEmptyAttribute extends BooleanAttribute
+{
+    public IsEmptyAttribute(Boolean value)
+    {
+        super(value);
+    }
+
+    @Override
+    public Boolean getValue(ItemStack itemStack)
+    {
+        return isEmpty(itemStack);
+    }
+    
+    public static Boolean isEmpty(ItemStack stack)
+    {
+        AtomicBoolean empty = new AtomicBoolean(false);
+        if(stack.getItem() instanceof BackpackItem) {
+            stack.getCapability(CapabilityBackpackWrapper.getCapabilityInstance()).ifPresent(iBackpackWrapper -> {
+                empty.set(((InventoryHandlerSlotTrackerAccessor) iBackpackWrapper.getInventoryHandler().getSlotTracker()).getEmptySlots().size() == ((BackpackItem) stack.getItem()).getNumberOfSlots());
+            });
+        }
+        
+        return empty.get();
+    }
+
+    @Override
+    public String getTranslationKey()
+    {
+        return "is_empty";
+    }
+
+    @Override
+    public String getLegacyKey()
+    {
+        return "empty";
+    }
+}
