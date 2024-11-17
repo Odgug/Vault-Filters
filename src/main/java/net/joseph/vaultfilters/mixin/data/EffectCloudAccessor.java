afe6294b0a572226f5a5ede39e96ca15cd6c278a
Kyle Robertson <me@iwolfking.xyz>
2024-11-16 20:03:12 -0600

    Update 16 Compat

--- src/main/java/net/joseph/vaultfilters/mixin/data/EffectCloudAccessor.java
+++ src/main/java/net/joseph/vaultfilters/mixin/data/EffectCloudAccessor.java
@@ -1,6 +1,6 @@
 package net.joseph.vaultfilters.mixin.data;
 
-import iskallia.vault.gear.attribute.custom.EffectCloudAttribute;
+import iskallia.vault.gear.attribute.custom.effect.EffectCloudAttribute;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
