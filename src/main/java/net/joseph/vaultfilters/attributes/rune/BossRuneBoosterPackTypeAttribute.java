e35495bcf880f24140d5a0f297779420859ffcc7
TinoMartino094 <78217336+TinoMartino094@users.noreply.github.com>
2025-06-19 02:23:25 -0500

    Add files via upload

--- src/main/java/net/joseph/vaultfilters/attributes/rune/BossRuneBoosterPackTypeAttribute.java
+++ src/main/java/net/joseph/vaultfilters/attributes/rune/BossRuneBoosterPackTypeAttribute.java
@@ -6,8 +6,8 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.ItemStack;
 
 public class BossRuneBoosterPackTypeAttribute extends StringAttribute {
     public BossRuneBoosterPackTypeAttribute(String value) {
@@ -21,17 +21,14 @@ public class BossRuneBoosterPackTypeAttribute extends StringAttribute {
         ListTag items = itemStack.getTag().getList("Items", 10);
         if (items.isEmpty()) return null;
         CompoundTag packTag = items.getCompound(0);
-        if (!"the_vault:booster_pack".equals(packTag.getString("id"))) return null;
         if (!packTag.contains("tag", 10)) return null;
         CompoundTag innerTag = packTag.getCompound("tag");
         if (!innerTag.contains("id")) return null;
-        String boosterId = innerTag.getString("id");
-        return boosterId;
+        return innerTag.getString("id");
     }
 
     @Override
     public Object[] getTranslationParameters() {
-        // Display the pack type's item name if possible
         String displayName = this.value;
         try {
             ResourceLocation id = new ResourceLocation(this.value);
--- src/main/java/net/joseph/vaultfilters/attributes/rune/BossRuneGearRarityAttribute.java
+++ src/main/java/net/joseph/vaultfilters/attributes/rune/BossRuneGearRarityAttribute.java
@@ -1,18 +1,10 @@
 package net.joseph.vaultfilters.attributes.rune;
 
-import iskallia.vault.gear.data.VaultGearData;
-import iskallia.vault.gear.item.VaultGearItem;
 import iskallia.vault.item.BossRuneItem;
 import net.joseph.vaultfilters.attributes.abstracts.StringAttribute;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Locale;
 
 public class BossRuneGearRarityAttribute extends StringAttribute {
     public BossRuneGearRarityAttribute(String value) {
@@ -26,25 +18,12 @@ public class BossRuneGearRarityAttribute extends StringAttribute {
         ListTag items = itemStack.getTag().getList("Items", 10);
         if (items.isEmpty()) return null;
         CompoundTag gearTag = items.getCompound(0);
-        if (!gearTag.contains("id")) return null;
-
-        String itemId = gearTag.getString("id");
-        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemId));
-        if (item == null) return null;
-        int count = gearTag.contains("Count") ? gearTag.getInt("Count") : 1;
-        ItemStack gearStack = new ItemStack(item, count);
-        if (gearTag.contains("tag", 10)) {
-            gearStack.setTag(gearTag.getCompound("tag"));
-        }
-        if (!(gearStack.getItem() instanceof VaultGearItem)) return null;
-        VaultGearData data = VaultGearData.read(gearStack);
-        return StringUtils.capitalize(data.getRarity().toString().toLowerCase(Locale.ROOT));
-    }
-
-    @Override
-    public Object[] getTranslationParameters() {
-        // Only show the rarity
-        return new Object[]{this.value};
+        if (!gearTag.contains("tag", 10)) return null;
+        CompoundTag innerTag = gearTag.getCompound("tag");
+        if (!innerTag.contains("clientCache", 10)) return null;
+        CompoundTag clientCache = innerTag.getCompound("clientCache");
+        if (!clientCache.contains("rollType")) return null;
+        return clientCache.getString("rollType");
     }
 
     @Override
