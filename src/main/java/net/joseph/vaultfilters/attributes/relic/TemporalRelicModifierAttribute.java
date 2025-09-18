1f2edccca3fd83dc1ce0785dd4892ec6755eced7
iwolfking <thegreatiwk@gmail.com>
2025-09-18 16:06:20 -0500

    Add Temporal Relic modifier attribute (plus fix runClient missing Patchouli dependency)

---
+++ src/main/java/net/joseph/vaultfilters/attributes/relic/TemporalRelicModifierAttribute.java
@@ -0,0 +1,33 @@
+package net.joseph.vaultfilters.attributes.relic;
+
+import iskallia.vault.core.vault.modifier.spi.VaultModifier;
+import iskallia.vault.item.gear.TemporalShardItem;
+import net.joseph.vaultfilters.attributes.abstracts.StringAttribute;
+import net.minecraft.world.item.ItemStack;
+
+public class TemporalRelicModifierAttribute extends StringAttribute {
+    public TemporalRelicModifierAttribute(String value) {
+        super(value);
+    }
+
+    @Override
+    public String getValue(ItemStack stack) {
+        if(!(stack.getItem() instanceof TemporalShardItem)) {
+            return null;
+        }
+
+        VaultModifier<?> modifier = TemporalShardItem.getVaultModifier(stack);
+
+        if(modifier == null) {
+            return null;
+        }
+
+
+        return modifier.getDisplayName();
+    }
+
+    @Override
+    public String getTranslationKey() {
+        return "temporal_relic_modifier";
+    }
+}
