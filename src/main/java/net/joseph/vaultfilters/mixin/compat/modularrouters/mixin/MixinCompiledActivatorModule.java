29380d1de92c6e77b22c0ecaebd9a41f1449d4c3
Odgug <108800252+Odgug@users.noreply.github.com>
2025-10-07 20:39:28 +0300

    fixed pouches requiring to be whitelisted + no longer need to be opened

--- src/main/java/net/joseph/vaultfilters/mixin/compat/modularrouters/mixin/MixinCompiledActivatorModule.java
+++ src/main/java/net/joseph/vaultfilters/mixin/compat/modularrouters/mixin/MixinCompiledActivatorModule.java
@@ -1,5 +1,6 @@
 package net.joseph.vaultfilters.mixin.compat.modularrouters.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.simibubi.create.content.logistics.filter.FilterItemStack;
 import iskallia.vault.item.BoosterPackItem;
 import iskallia.vault.item.JewelPouchItem;
@@ -10,10 +11,12 @@ import me.desht.modularrouters.logic.compiled.CompiledActivatorModule;
 import me.desht.modularrouters.logic.filter.matchers.BulkItemMatcher;
 import me.desht.modularrouters.logic.filter.matchers.IItemMatcher;
 import me.desht.modularrouters.logic.filter.matchers.SimpleItemMatcher;
+import net.joseph.vaultfilters.VaultFilters;
 import net.joseph.vaultfilters.attributes.old.CardPackChooseAttribute;
 import net.joseph.vaultfilters.attributes.old.JewelPouchChooseAttribute;
 import net.joseph.vaultfilters.items.VFItems;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.util.FakePlayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,21 +26,61 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.Objects;
 
 @Mixin(value = CompiledActivatorModule.class, remap = false)
 public abstract class MixinCompiledActivatorModule {
 
+    @ModifyExpressionValue(
+            method = "execute",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/desht/modularrouters/logic/filter/Filter;test(Lnet/minecraft/world/item/ItemStack;)Z"
+            )
+    )
+    private boolean activateOnPacks1(boolean original,ModularRouterBlockEntity router) {
+        if (vault_Filters$shouldChooseJewel(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers()) && router.getBufferItemStack().getItem() instanceof JewelPouchItem) {
+            return true;
+        }
+        if (vault_Filters$shouldChooseCard(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers()) && router.getBufferItemStack().getItem() instanceof BoosterPackItem)  {
+            return true;
+        }
+        return original;
+    }
+
+    @ModifyExpressionValue(
+            method = "execute",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/desht/modularrouters/logic/filter/Filter;isBlacklist()Z"
+            )
+    )
+    private boolean activateOnPacks2(boolean original,ModularRouterBlockEntity router) {
+        if (vault_Filters$shouldChooseJewel(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers()) && router.getBufferItemStack().getItem() instanceof JewelPouchItem) {
+            return true;
+        }
+        if (vault_Filters$shouldChooseCard(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers()) && router.getBufferItemStack().getItem() instanceof BoosterPackItem)  {
+            return true;
+        }
+        return original;
+    }
 
     @Inject(method = "doUseItem", at = @At("HEAD"), cancellable = true)
     private void vaultOpenables(ModularRouterBlockEntity router, FakePlayer fakePlayer, CallbackInfoReturnable<Boolean> cir) {
         ItemStack routerStack = router.getBufferItemStack();
-        if (routerStack.getItem() instanceof BoosterPackItem) {
+        if (routerStack.getItem() instanceof BoosterPackItem packItem) {
             if (!vault_Filters$shouldChooseCard(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers())) {
                 return;
             }
             var outcomes = BoosterPackItem.getOutcomes(routerStack);
+            //boolean crackedPack = false;
             if (outcomes == null || outcomes.isEmpty()) {
-                return;
+                if (router.getLevel() != null) {
+                    InteractionResultHolder<ItemStack> result = packItem.use(router.getLevel(), fakePlayer, InteractionHand.MAIN_HAND);
+                    //fakePlayer.setItemInHand(InteractionHand.MAIN_HAND,result.getObject().copy());
+                    outcomes = BoosterPackItem.getOutcomes(routerStack);
+                    //crackedPack = true;
+                }
             }
             var filter = ((AccessorCompiledModule) this).getFilter();
             for (ItemStack outcome : outcomes) {
@@ -47,15 +90,25 @@ public abstract class MixinCompiledActivatorModule {
                     return;
                 }
             }
+//            if (crackedPack) {
+//                cir.setReturnValue(true);
+//                return;
+//            }
         }
 
-        if (routerStack.getItem() instanceof JewelPouchItem) {
+        if (routerStack.getItem() instanceof JewelPouchItem pouchItem) {
             if (!vault_Filters$shouldChooseJewel(((AccessorFilter)((AccessorCompiledModule) this).getFilter()).getMatchers())) {
                 return;
             }
             var outcomes = JewelPouchItem.getJewels(routerStack);
+            //boolean crackedJewel = false;
             if (outcomes.isEmpty()) {
-                return;
+                if (router.getLevel() != null) {
+                    InteractionResultHolder<ItemStack> result = pouchItem.use(router.getLevel(), fakePlayer, InteractionHand.MAIN_HAND);
+                    //fakePlayer.setItemInHand(InteractionHand.MAIN_HAND,result.getObject().copy());
+                    outcomes = JewelPouchItem.getJewels(routerStack);
+                    //crackedJewel = true;
+                }
             }
             var filter = ((AccessorCompiledModule) this).getFilter();
             for (JewelPouchItem.RolledJewel outcome : outcomes) {
@@ -70,6 +123,10 @@ public abstract class MixinCompiledActivatorModule {
                     return;
                 }
             }
+//            if (crackedJewel) {
+//                cir.setReturnValue(true);
+//                return;
+//            }
         }
     }
 
