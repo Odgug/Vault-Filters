1c74ef5fe98e4bf489d52d66a878a321899cdebb
Odgug <108800252+Odgug@users.noreply.github.com>
2025-10-13 01:22:44 +0300

    version bump + mixin fix (untested cant build)

--- src/main/java/net/joseph/vaultfilters/mixin/other/MixinAttributeResearchBypass.java
+++ src/main/java/net/joseph/vaultfilters/mixin/other/MixinAttributeResearchBypass.java
@@ -3,23 +3,24 @@ package net.joseph.vaultfilters.mixin.other;
 import com.simibubi.create.content.logistics.filter.FilterItem;
 import iskallia.vault.research.ResearchTree;
 import iskallia.vault.research.Restrictions;
+import iskallia.vault.research.type.Research;
 import net.joseph.vaultfilters.configs.VFServerConfig;
 import net.joseph.vaultfilters.items.SelectItemAugment;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 //Create filter integration for modular routers by radimous on GitHub rizek_ on Discord, massive thanks.
 
 @Mixin(value = ResearchTree.class, remap = false)
 public class MixinAttributeResearchBypass {
-    @Inject(method = "restrictedBy(Lnet/minecraft/world/item/ItemStack;Liskallia/vault/research/Restrictions$Type;)Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
-    public void researchOverride(ItemStack item, Restrictions.Type restrictionType, CallbackInfoReturnable<String> cir) {
-        if (item.getItem() instanceof FilterItem || item.getItem() instanceof SelectItemAugment){
+    @Redirect(method = "restrictedBy(Lnet/minecraft/world/item/ItemStack;Liskallia/vault/research/Restrictions$Type;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Liskallia/vault/research/type/Research;restricts(Lnet/minecraft/world/item/ItemStack;Liskallia/vault/research/Restrictions$Type;)Z"))
+    public boolean researchOverride(Research instance, ItemStack stack, Restrictions.Type type) {
+        if (stack.getItem() instanceof FilterItem || stack.getItem() instanceof SelectItemAugment){
             String name = VFServerConfig.RESEARCH_NAME.get();
-            cir.setReturnValue(name.isBlank() ? null : name);
+            return instance.getName().equals(name);
         }
+        return instance.restricts(stack,type);
     }
 }
