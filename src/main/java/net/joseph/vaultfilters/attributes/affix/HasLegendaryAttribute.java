85a4b2ab96d0d4828163bd00897cda0e57d35fd8
Odgug <108800252+Odgug@users.noreply.github.com>
2025-10-16 06:58:52 +0300

    fixed legendary unidentified gear

--- src/main/java/net/joseph/vaultfilters/attributes/affix/HasLegendaryAttribute.java
+++ src/main/java/net/joseph/vaultfilters/attributes/affix/HasLegendaryAttribute.java
@@ -1,8 +1,10 @@
 package net.joseph.vaultfilters.attributes.affix;
 
+import iskallia.vault.gear.attribute.VaultGearAttribute;
 import iskallia.vault.gear.attribute.VaultGearModifier;
 import iskallia.vault.gear.data.VaultGearData;
 import iskallia.vault.gear.item.VaultGearItem;
+import iskallia.vault.init.ModGearAttributes;
 import net.joseph.vaultfilters.attributes.abstracts.BooleanAttribute;
 import net.minecraft.world.item.ItemStack;
 
@@ -22,6 +24,9 @@ public class HasLegendaryAttribute extends BooleanAttribute {
             if (modifier.hasCategory(VaultGearModifier.AffixCategory.LEGENDARY)) {
                 return true;
             }
+        }
+        if (data.hasAttribute(ModGearAttributes.IS_LEGENDARY)) {
+            return true;
         }
 
         return false;
